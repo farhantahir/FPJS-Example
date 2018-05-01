09a067f274fbb3f8a9bf7721f161b1f6d826400d
farhan <mf.tahir@outlook.com>
2018-05-01 22:51:18 +0500

    Added form, actions and action types as well added Update to update model

--- src/Update.js
+++ src/Update.js
@@ -1,5 +1,59 @@
+import * as R from 'ramda';
+
+const ACTION_TYPES = {
+	SHOW_FORM: "SHOW_FORM",
+	MEAL_INPUT: "MEAL_INPUT",
+	CALORIES_INPUT: "CALORIES_INPUT"
+};
+
+export function showFormAction(showForm) {
+	return {
+		type: ACTION_TYPES.SHOW_FORM,
+		data: {
+			showForm
+		}
+	};
+}
+
+export function mealsInputAction(description) {
+	return {
+		type: ACTION_TYPES.MEAL_INPUT,
+		data: {
+			description
+		}
+	};	
+}
+
+export function caloriesInputAction(calories) {
+	return {
+		type: ACTION_TYPES.CALORIES_INPUT,
+		data: {
+			calories
+		}
+	};	
+}
+
+
 function update(action, model) {
-	return model;
+	switch(action.type) {
+		case ACTION_TYPES.SHOW_FORM: 
+			const { data: { showForm } } = action;
+			return { ...model, showForm, description: '', calories: 0 };
+
+		case ACTION_TYPES.MEAL_INPUT:
+			const { data: { description } }	= action;
+			return { ...model, description };
+
+		case ACTION_TYPES.CALORIES_INPUT:
+			const calories = R.pipe(
+				parseInt,
+				R.defaultTo(0)
+			)(action.data.calories);
+			return { ...model, calories };
+
+		default:
+			return model;	
+	}
 }
 
 export default update;
--- src/View.js
+++ src/View.js
@@ -1,15 +1,21 @@
 import hh from 'hyperscript-helpers';
 import { h } from 'virtual-dom';
+import { 
+	showFormAction,
+	mealsInputAction,
+	caloriesInputAction
+} from './Update';
 
 const { pre, div, h1, button, form, label, input } = hh(h);
 
-function fieldSet(labelText, inputValue) {
+function fieldSet(labelText, inputValue, oninput) {
 	return div([
 		label({ className: 'db mb1' }, labelText),
 		input({  
 			className: 'pa2, input-reset ba w-100 mb2',
 			type: 'text',
-			value: inputValue
+			value: inputValue,
+			oninput
 		})
 	]);
 }
@@ -26,7 +32,8 @@ function buttonSet(dispatch) {
 		button(
 			{
 				className: 'f3 pv2 ph3 bg-light-gray bn mr2 dim',
-				type: 'Submit'
+				type: 'button',
+				onclick: () => dispatch(showFormAction(false))
 			},
 			'Cancel'
 		)
@@ -34,20 +41,27 @@ function buttonSet(dispatch) {
 }
 
 function formView(dispatch, model) {
-  const {descriptoin, calories, showForm} = model;
+  const {description, calories, showForm} = model;
 	if (showForm) {
 		return form(
 			{ className: 'w-100 mv2' },
 			[
-			 fieldSet('Meal', descriptoin),
-			 fieldSet('Calories', calories || ''),
+			 fieldSet('Meal', description, 
+			 	e => dispatch(mealsInputAction(e.target.value))
+			 ),
+			 fieldSet('Calories', calories || '',
+			 	e => dispatch(caloriesInputAction(e.target.value))
+		 	 ), 
 			 buttonSet(dispatch)
 			]
 		);	
 	}
 	
 	return button(
-		{className: 'f3 pv2 ph3 bn bg-blue white'},
+		{
+			className: 'f3 pv2 ph3 bn bg-blue white',
+			onclick: () => dispatch(showFormAction(true)),
+		},	
 		'Add Meal'
 	);
 }
